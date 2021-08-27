55cb9a6d821a13700d087cc2ccee06383d0eabad
KDraxler <39791072+KDraxler@users.noreply.github.com>
2021-08-28 03:05:10 +0700

    update major li

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-window.onscroll = function() {scrollFunction()};
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav .nav-items .menu ul li');
 
+
+window.addEventListener('scroll', ()=> {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        console.log(sectionHeight)
+        
+        if(pageYOffset >= (sectionTop - sectionHeight / 1.85)){
+            current = section.getAttribute('id');
+        }
+    })
+
+    navLi.forEach( li =>{
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active')
+        }
+    })
+})
+
+window.onscroll = function() {scrollFunction()};
 
 $(document).ready(function(){
     ngitung(10,300);
 });
 
-
-$(Document).on('click', 'ul li', function(){
-    $(this).addClass('active').siblings().removeClass('active');
-})
+// $(document).on('click', 'ul li', function(){
+//     $(this).addClass('active').siblings().removeClass('active');
+// })
 
 function scrollFunction() {
-    
-    let itung = document.getElementsByClassName("tc3");
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         document.getElementById("navbar").style.boxShadow = "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px";
     }
