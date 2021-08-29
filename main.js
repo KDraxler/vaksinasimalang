d369177b5db56c4892d0da969aa4fac852d5a2dd
KDraxler <39791072+KDraxler@users.noreply.github.com>
2021-08-30 06:39:29 +0700

    add random lokasi vaksin

--- main.js
+++ main.js
@@ -48,3 +48,26 @@ function ngitung(d,t){
     $(".num").counterUp({delay:d, time:t})
     
 }
+
+function shuffle(array) {
+    var currentIndex = array.length,
+    temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+var categories = shuffle($(".lv3>div").get());
+$(".lv3").html(categories);
